7339e66605a9ab7815eb7b85888f5f74e9f89e60
Harsh Sharma <sharmaharsh177@gmail.com>
2018-11-09 18:56:07 +0530

    email and final project

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -13,12 +13,59 @@ $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
-// Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
+// // Create the email and send the message
+// $to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+// $email_subject = "Website Contact Form:  $name";
+// $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+// $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+// $headers .= "Reply-To: $email_address";	
+// mail($to,$email_subject,$email_body,$headers);
+// return true;			
+
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'Exception.php';
+require 'PHPMailer.php';
+require 'SMTP.php';
+
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'sakshinagardesigns@gmail.com';                 // SMTP username
+    $mail->Password = 'Sakshi@123';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('sakshinagardesigns@gmail.com', 'Sakshi Nagar');
+    $mail->addAddress('int.sakshi@gmail.com', 'Sakshi Nagar');     // Add a recipient
+    //$mail->addAddress('ellen@example.com');               // Name is optional
+    //$mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = "Website Contact Form:  $name";
+    $mail->Body    = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name, \n\nEmail: $email_address\n\n, Message:\n$message";
+    $mail->AltBody = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name, \n\nEmail: $email_address\n\n, Message:\n$message";
+
+    $mail->send();
+    //echo 'Message has been sent';
+    return true;
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+}
 ?>
